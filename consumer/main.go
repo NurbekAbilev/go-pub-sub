42e739b11a2b3e2fed26702fa4ff5388774b8d4b
Nurbek Abilev <abilev.nurbek@gmai.com>
2025-04-08 02:32:24 +0500

    adsf

--- consumer/main.go
+++ consumer/main.go
@@ -1,64 +1,84 @@
 package main
 
 import (
-	"context"
 	"log"
 	"os"
-	"strings"
-	"time"
 
-	"github.com/rabbitmq/amqp091-go"
-	_ "github.com/rabbitmq/amqp091-go"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
-	conn, err := amqp091.Dial("amqp://user:password@localhost:5672/")
-	failOnError(err, "failed to connect to rabbit")
+	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	failOnError(err, "failed to ope channel")
+	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	err = ch.ExchangeDeclare(
+		"logs",
+		"fanout",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed echange declare")
+
 	q, err := ch.QueueDeclare(
-		"hello", false, false, false, false, nil,
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
 	)
-	failOnError(err, "failed to declare a queue")
+	failOnError(err, "Failed to declare a queue")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	body := bodyFrom(os.Args)
-	err = ch.PublishWithContext(
-		ctx,
-		"",
-		q.Name,
-		false,
-		false,
-		amqp091.Publishing{
-			ContentType: "text/plain",
-			Body:        []byte(body),
-		},
-	)
-
-	failOnError(err, "failed to publish a message")
-	log.Printf(" [x] Sent %s\n", body)
-
-}
-
-func bodyFrom(args []string) string {
-	var s string
-	if len(args) < 2 || os.Args[0] == "" {
-		s = "Hello"
-	} else {
-		s = strings.Join(args[1:], " ")
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [info] [warning] [error]", os.Args[0])
+		os.Exit(0)
 	}
 
-	return s
+	for _, s := range os.Args[1:] {
+		log.Printf("Binding queue %s to exchange %s with routing key %s",
+			q.Name, "logs_direct", s)
+		err = ch.QueueBind(
+			q.Name,        // queue name
+			s,             // routing key
+			"logs_direct", // exchange
+			false,
+			nil)
+		failOnError(err, "Failed to bind a queue")
+	}
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
 
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Panicf("%s: %s\n", msg, err)
+		log.Panicf("%s: %s", msg, err)
 	}
 }
--- producer/main.go
+++ producer/main.go
@@ -1,74 +1,81 @@
 package main
 
 import (
-	"bytes"
+	"context"
 	"log"
+	"os"
+	"strings"
 	"time"
 
+	// _ "github.com/rabbitmq/amqp091-go"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	conn, err := amqp.Dial("amqp://user:password@localhost:5672/")
-	failOnError(err, "Failed to connect to RabbitMQ")
+	failOnError(err, "failed to connect to rabbit")
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
+	failOnError(err, "failed to ope channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.Qos(1, 0, false)
-	failOnError(err, "failed to set qos")
-
-	msgs, err := ch.Consume(
-		q.Name,
-		"",
-		false,
+	err = ch.ExchangeDeclare(
+		"logs_direct",
+		"direct",
+		true,
 		false,
 		false,
 		false,
 		nil,
 	)
-	failOnError(err, "failed to register a consumer")
+	failOnError(err, "failed echange declare")
 
-	var forever chan struct{}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	go func() {
-		for d := range msgs {
-			log.Printf("received a message: %s", d.Body)
-			dotCount := bytes.Count(d.Body, []byte("."))
-			t := time.Duration(dotCount)
-			time.Sleep(t * time.Second)
-			// if rand.Float64() > 0.5 {
-			// 	log.Printf("acking message")
-			// 	d.Ack(false)
-			// } else {
-			// 	log.Printf("NACKING message")
-			// 	d.Nack(false, true)
-			// 	continue
-			// }
-			d.Ack(false)
-			log.Printf("Done")
-			// d.Ack(false)
-		}
-	}()
+	body := bodyFrom(os.Args)
+	err = ch.PublishWithContext(
+		ctx,
+		"logs_direct",
+		severityFrom(os.Args),
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
 
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
+	failOnError(err, "failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+
+}
+
+func bodyFrom(args []string) string {
+	var s string
+	if len(args) < 2 || os.Args[0] == "" {
+		s = "Hello"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+
+	return s
+}
+
+func severityFrom(args []string) string {
+	var s string
+	if len(args) < 2 || args[1] == "" {
+		s = "info"
+	} else {
+		s = os.Args[1]
+	}
+
+	return s
 }
 
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Panicf("%s: %s", msg, err)
+		log.Panicf("%s: %s\n", msg, err)
 	}
 }
